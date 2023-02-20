5432fa322aa3a5fd2bec0715f75d7761599b0bdb
Emrys Grant <69246111+emrys775@users.noreply.github.com>
2023-02-20 21:06:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+  print("You can ridethe rollercoaster!")
+  
+  age = int(input("What is your age?"))
+
+  if age < 12:
+    print("Please pay $5.0")
+  elif age <= 18:
+    print("Please pay $7.0")
+  else:
+    print("Please pay $12.0")
+
+  wants_photo = ("Do you want photo taken? Y or N")
+  if wants_photo == "Y":
+    
+  
+  
+
+  
+       
+else:
+  print("Sorry!, youhave to grow taller before you can ride")
